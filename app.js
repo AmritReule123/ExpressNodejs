52a100d32e336a553994f8a832759006c17eb1a8
Amrit Reule <reuleamrit47@gmail.com>
2026-02-01 11:01:34 +0545

    code change hota hyaa time par mera dost

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Welocomet ot jss ")
+
+
+
+const a = 4;
+b = 7;
+let SUM = a+ b;
+
+console.log(a + b);
+console.log(SUM);
+
+
+
+// Array
+const numbers = [1,2,3,4,5];
+// console.log(numbers.length)
+// console.log(numbers[2])
+
+// OBJECTS
+const person = {
+    name: "amrit",
+    age: 33,
+    hobbies:['Reading', 'Travelling', 'programming']
+}
+console.log(person)
+console.log(person.age)
---
+++ day4.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express is working 🎉");
+});
+
+app.listen(3000, () => console.log("i ama runnign a code in  3000"));
