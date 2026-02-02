33cd84b5ff65fc350822fc7103afe8253e5c78e6
Amrit Reule <reuleamrit47@gmail.com>
2026-02-02 20:28:35 +0545

    i am changer in code in day5

--- day5.js
+++ day5.js
@@ -1,14 +1,19 @@
 const express = require("express");
-const app = express();
+const ram = express();
 
 
 //alternative method ///
 // let app = require("express")();
 
 
+ram.get("/", (music,song)=>{
+    song.send("songs are widely listen and love dance")
+})
 
+ram.get("/about", (music,song)=>{
+    song.send("car myaa music bachaa oww")
+})
 
-
-app.listen(3100, function(){
-    console.log("this is a javascript language server")
+ram.listen(3200, function(){
+    console.log("this is a javascript dnfc,dsn ,sndf,ndsn msdmsd.fm language server")
 })
